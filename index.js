409728b104bed274c0d4816eab8b50d3d488fdc2
sayedariful <sayedariful73@gmail.com>
2023-10-27 06:22:23 +0600

    car doctore server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middle ware
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS)
+
+// console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njefots.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,29 +21,39 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db("carDoctor").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('doctor is running')
-})
+app.get("/", (req, res) => {
+  res.send("doctor is running");
+});
 
 app.listen(port, (req, res) => {
-    console.log(`Car doctor server is running on port ${port}`);
+  console.log(`Car doctor server is running on port ${port}`);
 });
